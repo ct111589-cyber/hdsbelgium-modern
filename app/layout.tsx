eeeb317972a7728c21ececeac5e63f5dd91fed77
ct111589-cyber <ct111589@gmail.com>
2026-01-30 14:16:58 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 // app/layout.tsx
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // This layout just passes children down to the [locale] layout
   return children;
 }
