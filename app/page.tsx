f89116705f5c628d5088e13159147fb31a7773bf
ct111589-cyber <ct111589@gmail.com>
2026-01-30 14:01:42 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,6 @@
-export { default } from './[locale]/page';
+import {redirect} from 'next/navigation';
+
+// This redirects "/" to "/en" (or your default locale)
+export default function RootPage() {
+  redirect('/en'); 
+}
